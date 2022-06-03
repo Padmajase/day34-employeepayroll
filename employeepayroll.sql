4d1ac6e841326e28fbb1cc980e99f58ed5ff5d68
Padmajase <padmajapawar7@gmail.com>
2022-06-02 21:26:01 -0700

    Retrieved : employee payroll data i.e. UC4

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -8,3 +8,4 @@ desc employee_payroll;
 
  -- creating employee payroll data 
 insert into employee_payroll (name, salary, start_date) values('neha', 30000, 7/1/2021);
+select * from employee_payroll;
