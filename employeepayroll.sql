1dc294f1f50c5eb07cbc59243b4725d00ce0cb1a
Padmajase <padmajapawar7@gmail.com>
2022-06-02 21:24:45 -0700

    Inserted : employee payroll data i.e. UC3

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -5,3 +5,6 @@ use payroll_service;
 create table employee_payroll(id int auto_increment not null, name varchar(20), salary bigint, start_date date, primary key (id));
 show tables;
 desc employee_payroll;
+
+ -- creating employee payroll data 
+insert into employee_payroll (name, salary, start_date) values('neha', 30000, 7/1/2021);
