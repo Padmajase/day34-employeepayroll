188e3cf063a6db4242bbbbb0187d2e629b91ac45
Padmajase <padmajapawar7@gmail.com>
2022-06-02 22:58:16 -0700

    Retrieved : employee payroll data using where clause i.e. UC5

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -7,5 +7,8 @@ show tables;
 desc employee_payroll;
 
  -- creating employee payroll data 
-insert into employee_payroll (name, salary, start_date) values('neha', 30000, 7/1/2021);
+insert into employee_payroll (name, salary, start_date) values('shivpooja', 45000, '2012-09-05');
 select * from employee_payroll;
+
+select salary from employee_payroll where name='neha';
+select name from employee_payroll where start_date between cast('2012-09-2' as date) and date (now());
