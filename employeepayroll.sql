4f190f12915f33abff6ee6834225fea866a2a121
Padmajase <padmajapawar7@gmail.com>
2022-06-02 23:08:16 -0700

    Updated : employee payroll data using update i.e. UC6

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -12,3 +12,7 @@ select * from employee_payroll;
 
 select salary from employee_payroll where name='neha';
 select name from employee_payroll where start_date between cast('2012-09-2' as date) and date (now());
+
+-- adding  column gender to table
+alter table employee_payroll add gender char after name ;
+update employee_payroll set gender='M' where name='rohit' or name='punit' or name='mahi';
