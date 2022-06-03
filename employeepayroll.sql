f5bd527c4d77cebdbf2e73548e32018f55ff6ab3
Padmajase <padmajapawar7@gmail.com>
2022-06-02 21:11:32 -0700

    Created : a table employee payroll i.e. UC2

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -1,3 +1,7 @@
 show databases;
 create database payroll_service;
-use payroll_service
+use payroll_service;
+
+create table employee_payroll(id int auto_increment not null, name varchar(20), salary bigint, start_date date, primary key (id));
+show tables;
+desc employee_payroll;
